1f0a663e98f8c38f61cbaa76a648cd6c3cf2b088
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-02-06 03:49:26 -0800

    add: ProductCard

--- src/components/elements/ProductCard.tsx
+++ src/components/elements/ProductCard.tsx
@@ -1,17 +1,37 @@
-import { ProductType } from "../../store/reducers/productsReducer";
+import { useDispatch } from "react-redux";
+import { ProductType } from "../blocks/ProductList/ProductList";
+import { FaPlus } from "react-icons/fa6";
+import {
+  selectProduct,
+  toggleProductModal,
+} from "../../store/actions/actionCreator";
 
 const ProductCard = ({
+  id,
   productPhoto,
   productName,
   productPrice,
 }: ProductType) => {
+  const dispatch = useDispatch();
+
+  const handleOpenModal = () => {
+    dispatch(selectProduct(id));
+    dispatch(toggleProductModal());
+  };
+
   return (
-    <div>
-      <img src={productPhoto} alt={productName} />
-      <h1 className="font-medium">{productName}</h1>
-      <div>
-        <h1>{productPrice}</h1>
-        <div className="border-2 border-gray-400 p-3 rounded-full">
+    <div className="w-40 lg:w-auto whitespace-nowrap overflow-x-clip">
+      <img
+        onClick={handleOpenModal}
+        className="w-40 rounded-xl md:w-80 cursor-pointer"
+        src={productPhoto}
+        alt={productName}
+      />
+      <h1 className="font-medium text-ellipsis">{productName}</h1>
+      <div className="block md:flex justify-between items-center">
+        <h1 className="font-medium">{productPrice}</h1>
+        <div className="border-2 border-gray-400 p-3 mt-1 rounded-full flex justify-center items-center gap-2 w-full cursor-pointer lg:w-fit">
+          <FaPlus size={20} />
           <button>Add to cart</button>
         </div>
       </div>
