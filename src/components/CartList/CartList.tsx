588be0b927d113b851cb1553615ecd215a7f6fe7
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-01 03:45:11 -0800

    add: create cart list component

---
+++ src/components/CartList/CartList.tsx
@@ -0,0 +1,37 @@
+import { useSelector } from "react-redux";
+import CartItem from "../elements/CartItem";
+import { RootState } from "../../store/state/rootReducers";
+
+const CartList = () => {
+  const cartList = useSelector((state: RootState) => state.cart?.cart);
+  const cart = localStorage.getItem("cart");
+  const parsedCart = cart ? JSON.parse(cart) : [];
+  return (
+    <div>
+      <div className="mt-3">
+        {parsedCart?.length === 0 && (
+          <h1 className="font-medium text-3xl text-gray-400 text-center">
+            Cart is currently empty :(
+          </h1>
+        )}
+        {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          parsedCart?.map((item: any) => (
+            <CartItem
+              id={item.product.id}
+              productComposition={item.product.productComposition}
+              productDescription={item.product.productDescription}
+              productName={item.product.productName}
+              productPhoto={item.product.productPhoto}
+              productPrice={item.product.productPrice}
+              restaurantID={item.product.restaurantID}
+              key={item.product.id}
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default CartList;
