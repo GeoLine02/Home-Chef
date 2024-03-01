3899742addb0d5ee42f02269611865f5cb3bc49b
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-01 03:41:02 -0800

    add: add redux devtools extension

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import "./index.css";
 import { legacy_createStore as createStore } from "redux";
 import rootReducers from "./store/state/rootReducers";
 import { Provider } from "react-redux";
-const store = createStore(rootReducers);
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(rootReducers, composeWithDevTools());
+
+store.subscribe(() => {
+  console.log("current state", store.getState());
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
