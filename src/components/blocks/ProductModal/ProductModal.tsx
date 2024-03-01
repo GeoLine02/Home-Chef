9d7b129098028d77cf579e7d6e4fabb566da2cdf
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-01 04:28:50 -0800

    add: add more props to ProductDetails component

--- src/components/blocks/ProductModal/ProductModal.tsx
+++ src/components/blocks/ProductModal/ProductModal.tsx
@@ -2,25 +2,30 @@ import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 import ProductDetails from "../../elements/ProductDetails";
 import { useSelector } from "react-redux";
-import { RootState } from "../../../store/state/rootReducers";
 
 type ProductModalType = {
   children: ReactNode;
+  handleQuantityIncrease: () => void;
+  handleQuantityDecrease: () => void;
 };
 
 const ProductModal = ({ children }: ProductModalType) => {
   const productDetails = useSelector(
-    (state: RootState) => state.products.selectedProduct
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (state: any) => state.products.selectedProduct
   );
   return createPortal(
-    <div className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center ">
+    <div className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center">
       <div>
         <ProductDetails
-          id={productDetails.id}
-          productDescription={productDetails.productDescription}
-          productName={productDetails.productName}
-          productPhoto={productDetails.productPhoto}
-          productPrice={productDetails.productPrice}
+          id={productDetails?.id}
+          productDescription={productDetails?.productDescription}
+          productComposition={productDetails?.productComposition}
+          productName={productDetails?.productName}
+          productPhoto={productDetails?.productPhoto}
+          productPrice={productDetails?.productPrice}
+          restaurantID={productDetails?.restaurantID}
+          key={productDetails?.id}
         />
       </div>
       <div>{children}</div>
