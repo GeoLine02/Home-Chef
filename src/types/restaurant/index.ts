945983a2aa1a3421413db4bb379504d9f04c1137
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-13 07:10:20 -0700

    add: create types for favorite restaurants

--- src/types/restaurant/index.ts
+++ src/types/restaurant/index.ts
@@ -9,3 +9,19 @@ export type RestaurantType = {
   phoneNumber: string;
   products: ProductType[];
 };
+
+export interface RestaurantResultType {
+  address: string;
+  city: string;
+  id: number;
+  name: string;
+  ownerId: number;
+  phoneNumber: string;
+}
+
+export interface IFavoriteRestaurants extends RestaurantResultType {
+  restaurantID: number;
+  createdAt: string;
+  updatedAt: string;
+  userID: number;
+}
