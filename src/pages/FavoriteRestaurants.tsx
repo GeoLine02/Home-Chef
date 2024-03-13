d52456588abfc87fa11c088928b795ccf25b897a
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-13 07:06:06 -0700

    add: fetch and dispaly FavoriteRestaurants

--- src/pages/FavoriteRestaurants.tsx
+++ src/pages/FavoriteRestaurants.tsx
@@ -1,26 +1,51 @@
-import { http } from "../helpers/http";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/state/rootReducers";
+import { saveFavoriteRestaurantsData } from "../store/actions/actionCreator";
+import { useEffect } from "react";
+import { getFavoriteRestaurants } from "../api/index";
+import RestaurantCard from "../components/elements/RestaurantCard";
+import { IFavoriteRestaurants } from "../types/restaurant";
+import RemoveFromFavorites from "../components/elements/buttons/RemoveFromFavorites";
 
 const FavoriteRestaurants = () => {
-  const fetchFavoriteRestaurants = async () => {
-    try {
-      const apiCallOptions = {
-        headers: {
-          "content-type": "application/json",
-        },
-        method: "GET",
-      };
-      const accessToken = localStorage.getItem("");
-      const res = await http(`/profile/favorites/${userId}`, apiCallOptions);
-      if (res.ok) {
-        const data = res.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.log("Favorite restaurants fetching error!", error);
+  const dispatch = useDispatch();
+  const userState = useSelector((state: RootState) => state.auth.userByID);
+  const userId = userState?.id;
+  const favoriteRestaurantsState = useSelector(
+    (state: RootState) => state.restaurants?.favoriteRestaurants
+  );
+  useEffect(() => {
+    if (userId) {
+      getFavoriteRestaurants(userId).then((favoriteRestaurantsList) => {
+        dispatch(saveFavoriteRestaurantsData(favoriteRestaurantsList));
+      });
     }
-  };
+  }, [dispatch, userId]);
 
-  return <div>FavoriteRestaurants</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-medium">Favorite Restaurants</h1>
+        <div className="flex gap-6 flex-wrap my-6">
+          {favoriteRestaurantsState?.map((restaurant: IFavoriteRestaurants) => (
+            <div key={restaurant.id} className="relative">
+              <RestaurantCard
+                id={restaurant.id}
+                name={restaurant.name}
+                address={restaurant.address}
+                city={restaurant.city}
+                ownerId={restaurant.ownerId}
+                phoneNumber={restaurant.phoneNumber}
+              />
+              <div className="absolute -top-3 -right-3">
+                <RemoveFromFavorites restaurantID={restaurant.restaurantID} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FavoriteRestaurants;
