cd478ad9cdf6d81a2764d336b4b7b434f28dce0f
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-13 07:06:39 -0700

    refactor: save restaurantById in redux state

--- src/pages/RestaurantByID.tsx
+++ src/pages/RestaurantByID.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa6";
 import ProductModal from "../components/blocks/ProductModal/ProductModal";
 import { RootState } from "../store/state/rootReducers";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Menu from "../components/elements/Menu";
 import SideCart from "../components/blocks/sideCart/SideCart";
 import { useEffect, useState } from "react";
@@ -11,22 +11,27 @@ import { http } from "../helpers/http";
 import routes from "../constants/routes";
 import UnfinishedOrderModal from "../components/blocks/UnfinishedOrderModal/UnfinishedOrderModal";
 import { text } from "../helpers/functions";
-import { RestaurantType } from "../types/restaurant";
+import { saveRestaurantByIdData } from "../store/actions/actionCreator";
+import RestaurantBanner from "../components/blocks/RestaurantBanner/RestaurantBanner";
 const RestaurantByID = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
   const cartState = useSelector((state: RootState) => state.cart.cart);
-  const [currentRestaurant, setCurrentRestaurant] = useState<RestaurantType>();
+  const dispatch = useDispatch();
+  const restaurantById = useSelector(
+    (state: RootState) => state.restaurants.restaurantById
+  );
+
   const [showUnfinishedOrderModal, setShowUnfinishedOrderModal] =
     useState<boolean>(false);
 
   useEffect(() => {
     const test =
       cartState?.length &&
-      cartState[0].product?.restaurantID !== currentRestaurant?.id;
+      cartState[0].product?.restaurantID !== restaurantById?.id;
     setShowUnfinishedOrderModal(test);
-  }, [cartState, currentRestaurant?.id]);
+  }, [cartState, restaurantById?.id]);
 
   useEffect(() => {
     const getRestaurantById = async () => {
@@ -41,57 +46,60 @@ const RestaurantByID = () => {
         const resp = await http(`/restaurant/${id}`, apiCallOptions);
         if (resp.statusText === "OK") {
           const data = await resp.json();
-          setCurrentRestaurant(data);
+          dispatch(saveRestaurantByIdData(data));
         }
       } catch (err) {
         console.log("restaurantByID data fetching error!", err);
       }
     };
     getRestaurantById();
-  }, [id]);
+  }, [id, dispatch]);
 
   const toggleProductModal = useSelector(
     (state: RootState) => state.products.toggleProductModal
   );
 
   return (
-    <div
-      className={
-        showUnfinishedOrderModal
-          ? "grid lg:grid-cols-repeat-5-18 mt-9 relative gap-6 justify-center blur-md"
-          : "grid lg:grid-cols-repeat-5-18 mt-9 relative gap-6 justify-center"
-      }
-    >
-      <div className="flex flex-col items-center">
-        <div
-          onClick={() => {
-            navigate(routes.home);
-          }}
-          className="hidden lg:flex lg:border-2 lg:border-gray-400 lg:rounded-full lg:p-3 lg:w-fit"
-        >
-          <FaArrowLeft size={20} />
-          <button>{text("RESTAURANT_BY_ID_BTN")}</button>
+    <div>
+      <RestaurantBanner />
+      <div
+        className={
+          showUnfinishedOrderModal
+            ? "grid lg:grid-cols-repeat-5-18 mt-9 relative gap-6 justify-center blur-md"
+            : "grid lg:grid-cols-repeat-5-18 mt-9 relative gap-6 justify-center"
+        }
+      >
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => {
+              navigate(routes.home);
+            }}
+            className="hidden lg:flex lg:border-2 lg:border-gray-400 lg:rounded-full lg:p-3 lg:w-fit"
+          >
+            <FaArrowLeft size={20} />
+            <button>{text("RESTAURANT_BY_ID_BTN")}</button>
+          </div>
+          <div className="flex flex-col items-cneter">
+            <h1 className="font-medium text-3xl hidden lg:block">
+              {text("COMMON_MENU")}
+            </h1>
+            <Menu />
+          </div>
         </div>
-        <div className="flex flex-col items-cneter">
-          <h1 className="font-medium text-3xl hidden lg:block">
-            {text("COMMON_MENU")}
-          </h1>
-          <Menu />
+        <div className="col-span-3 place-items-center">
+          <h1 className="font-medium text-3xl mb-6">*Dynamic Category*</h1>
+          <ProductList />
         </div>
-      </div>
-      <div className="col-span-3 place-items-center">
-        <h1 className="font-medium text-3xl mb-6">*Dynamic Category*</h1>
-        <ProductList />
-      </div>
-      {toggleProductModal && (
-        <div className="fixed top-0 left-0">
-          <ProductModal children />
+        {toggleProductModal && (
+          <div className="fixed top-0 left-0">
+            <ProductModal children />
+          </div>
+        )}
+        <div className="hidden lg:block">
+          <SideCart />
         </div>
-      )}
-      <div className="hidden lg:block">
-        <SideCart />
+        {showUnfinishedOrderModal ? <UnfinishedOrderModal children /> : null}
       </div>
-      {showUnfinishedOrderModal ? <UnfinishedOrderModal children /> : null}
     </div>
   );
 };
