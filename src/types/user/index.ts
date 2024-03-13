3b2bfdb5a2600151fdf9ddb1b0634fd7a815688e
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-13 07:11:05 -0700

    add: create type for userById

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -5,3 +5,25 @@ export interface IVKUserProfileData {
   role: string;
   isAccountActive: boolean;
 }
+
+export interface IGoogleProfileData {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: null | string;
+  role: string;
+  isAccountActive: boolean;
+  iat: number;
+}
+
+export interface IUserById {
+  address: string[];
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: null | string;
+  id: number;
+  isAccountActive: boolean;
+  role: string;
+}
