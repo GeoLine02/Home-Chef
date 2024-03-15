de63b79c097ae2ae49299327eca1ee16277888a7
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-15 05:50:48 -0700

    add test favorites

--- src/components/blocks/RestaurantBanner/RestaurantBanner.tsx
+++ src/components/blocks/RestaurantBanner/RestaurantBanner.tsx
@@ -1,14 +1,31 @@
+import { useSelector } from "react-redux";
 import RestaurantBannerImage from "../../../assets/RestaurantBanner.jpg";
 import AddToFavoritesBtn from "../../elements/buttons/AddToFavoritesBtn";
 import RestaurantInfoBtn from "../../elements/buttons/RestaurantInfoBtn";
+import { RootState } from "../../../store/state/rootReducers";
+import { useParams } from "react-router-dom";
 
 const RestaurantBanner = () => {
+  const favoriteRestaurants = useSelector(
+    (state: RootState) => state.restaurants?.favoriteRestaurants
+  );
+  const { id } = useParams();
+
+  console.log("dasda", favoriteRestaurants);
+
+  const addedRestaurant = favoriteRestaurants?.includes(
+    (restaurant: any) => restaurant.restaurantID === Number(id)
+  );
+
+  console.log(favoriteRestaurants);
+  console.log("add", addedRestaurant);
   return (
     <div className="relative">
       <img src={RestaurantBannerImage} alt="Restaurant banner" />
       <div className="flex absolute top-4 right-96 gap-4">
         <RestaurantInfoBtn />
-        <AddToFavoritesBtn />
+        {/* <AddToFavoritesBtn /> */}
+        {addedRestaurant ? <h1>added</h1> : <AddToFavoritesBtn />}
       </div>
     </div>
   );
--- src/components/elements/buttons/RemoveFromFavorites.tsx
+++ src/components/elements/buttons/RemoveFromFavorites.tsx
@@ -3,34 +3,57 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeFavoriteRestaurant } from "../../../api";
 import { RootState } from "../../../store/state/rootReducers";
 import { removeFavoriteRestaurantAction } from "../../../store/actions/actionCreator";
+import { Slide, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type RemoveFromFavoriesPropsType = {
   restaurantID: number;
 };
 
 const RemoveFromFavorites = ({ restaurantID }: RemoveFromFavoriesPropsType) => {
-  console.log("restaurantID", restaurantID);
   const dispatch = useDispatch();
   const userState = useSelector((state: RootState) => state.auth.userByID);
   const userID = userState?.id;
-  //   const favoriteRestaurantsState = useSelector(
-  //     (state: RootState) => state.restaurants.favoriteRestaurants
-  //   );
-  //   const favoriteRestaurantID = favoriteRestaurantsState?.find(
-  //     (restaurant: IFavoriteRestaurants) => restaurant.id === restaurantID
-  //   );
+  const favoriteRestaurants = useSelector(
+    (state: RootState) => state.restaurants.favoriteRestaurants
+  );
+  console.log(favoriteRestaurants);
+  const notification = () => toast.success("Restaurant removed successfully!");
   const handleRemoveFromFavorites = () => {
     removeFavoriteRestaurant(userID, restaurantID).then((removedRestaurant) => {
-      dispatch(removeFavoriteRestaurantAction(removedRestaurant));
+      if (removedRestaurant.removed)
+        dispatch(
+          removeFavoriteRestaurantAction(
+            favoriteRestaurants.filter(
+              (restaurant: any) => restaurant.restaurantID !== restaurantID
+            )
+          )
+        );
     });
+    notification();
   };
   return (
-    <button
-      onClick={handleRemoveFromFavorites}
-      className="bg-red-600 p-2 rounded-full"
-    >
-      <RiDeleteBin5Line size={20} color="white" />
-    </button>
+    <div>
+      <button
+        onClick={handleRemoveFromFavorites}
+        className="bg-red-600 p-2 rounded-full"
+      >
+        <RiDeleteBin5Line size={20} color="white" />
+      </button>
+      <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+    </div>
   );
 };
 
