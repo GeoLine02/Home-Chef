da3259721a698c87cc32418542fc0bdde7f6f891
GeoLine02 <126957977+GeoLine02@users.noreply.github.com>
2024-03-15 23:22:59 +0400

    add: adjust width and hait for different breakpoints

--- src/components/blocks/AuthModal/AuthModal.tsx
+++ src/components/blocks/AuthModal/AuthModal.tsx
@@ -9,7 +9,7 @@ type AuthModalProps = {
 const AuthModal = ({ children }: AuthModalProps) => {
   return createPortal(
     <div className="w-screen h-screen fixed top-0 left-0">
-      <div className="max-w-md fixed rounded-[12px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5">
+      <div className="w-screen h-screen md:max-w-md md:h-auto fixed rounded-[12px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5">
         <SignInForm />
         <div>{children}</div>
       </div>
