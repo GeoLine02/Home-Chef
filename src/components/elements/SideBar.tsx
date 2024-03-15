24f9e49b8b6ec94719a4d40ced7e093828c12f52
GeoLine02 <126957977+GeoLine02@users.noreply.github.com>
2024-03-15 23:29:22 +0400

    add: dynamic changing ui based on user authentication

--- src/components/elements/SideBar.tsx
+++ src/components/elements/SideBar.tsx
@@ -1,22 +1,18 @@
 import { AiOutlineClose } from "react-icons/ai";
-import { FaBars } from "react-icons/fa";
-import { TiLocationOutline } from "react-icons/ti";
-import { FaRegCreditCard } from "react-icons/fa6";
-import { FaRegHeart } from "react-icons/fa";
-import { FaRegQuestionCircle } from "react-icons/fa";
-import { GrLanguage } from "react-icons/gr";
-import { SlLogout } from "react-icons/sl";
-import { PiSignIn } from "react-icons/pi";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleBurger } from "../../store/actions/actionCreator";
-import { text } from "../../helpers/functions";
+import { RootState } from "../../store/state/rootReducers";
+import LoggedInUserSideBar from "./LoggedInUserSideBar";
+import LoggedOutUsesrSideBar from "./LoggedOutUsesrSideBar";
 
 const SideBar = () => {
   const dispatch = useDispatch();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const sideBarState = useSelector((state: any) => state.sideBar.isSideBarOpen);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const isUserLoggedIn = useSelector((state: any) => state.user.isUserLoggedIn);
+  const sideBarState = useSelector(
+    (state: RootState) => state.sideBar.isSideBarOpen
+  );
+  const isUserLoggedIn = useSelector(
+    (state: RootState) => state.auth.authUserVkInfo
+  );
   const handleToggleBurger = () => {
     dispatch(toggleBurger());
   };
@@ -33,52 +29,14 @@ const SideBar = () => {
           <div className="cursor-pointer">
             <AiOutlineClose size={30} />
           </div>
-          <div className="flex items-center gap-2 py-3">
-            <FaBars size={20} />
-            <p>{text("SIDEBAR_ORDER")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <TiLocationOutline size={20} />
-            <p>{text("SIDEBAR_ADDRESS")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <FaRegCreditCard size={20} />
-            <p>{text("SIDEBAR_CREDIT_CARD")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <FaRegHeart size={20} />
-            <p>{text("SIDEBAR_WISH_LIST")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <FaRegQuestionCircle size={20} />
-            <p>{text("SIDEBAR_NEED_HELP_QUESTION")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <GrLanguage size={20} />
-            <p>{text("SIDEBAR_LANGUAGE")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <SlLogout size={20} />
-            <p>{text("SIDEBAR_SIGN_OUT")}</p>
-          </div>
+          <LoggedInUserSideBar />
         </div>
       ) : (
         <div onClick={handleToggleBurger}>
           <div className="cursor-pointer">
             <AiOutlineClose size={30} />
           </div>
-          <div className="flex items-center gap-2 py-3">
-            <PiSignIn size={20} />
-            <p>{text("SIDEBAR_SIGN_IN")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <FaRegQuestionCircle size={20} />
-            <p>{text("SIDEBAR_NEED_HELP_QUESTION")}</p>
-          </div>
-          <div className="flex items-center gap-2 py-3">
-            <GrLanguage size={20} />
-            <p>{text("SIDEBAR_LANGUAGE")}</p>
-          </div>
+          <LoggedOutUsesrSideBar />
         </div>
       )}
     </div>
