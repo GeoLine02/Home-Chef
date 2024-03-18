96fd5d0a3f93331beb6748d1168903bdd5c0a0da
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-18 11:28:44 -0700

    add: add to favorites functionality

--- src/components/elements/buttons/AddToFavoritesBtn.tsx
+++ src/components/elements/buttons/AddToFavoritesBtn.tsx
@@ -3,31 +3,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { addFavoriteRestaurants } from "../../../api";
 import { addToFavorites } from "../../../store/actions/actionCreator";
 import { RootState } from "../../../store/state/rootReducers";
+import { IFavoriteRestaurants } from "../../../types/restaurant";
+import { GoHeartFill } from "react-icons/go";
 
 const AddToFavoritesBtn = () => {
   const dispatch = useDispatch();
-  const userState = useSelector((state: RootState) => state.auth.userByID);
-  console.log(userState);
-  
-  const userId = userState?.id;
+  const userState = useSelector(
+    (state: RootState) => state.auth?.authUserVkInfo
+  );
+  const favoriteRestaurants = useSelector(
+    (state: RootState) => state.restaurants?.favoriteRestaurants
+  );
   const restaurantByIdState = useSelector(
-    (state: RootState) => state.restaurants.restaurantById
+    (state: RootState) => state.restaurants?.restaurantById
   );
 
+  const userId = userState?.id;
   const restaurantId = restaurantByIdState.id;
+
+  const isRestaurantAdded = favoriteRestaurants?.some(
+    (restaurant: IFavoriteRestaurants) =>
+      restaurant.restaurantID === restaurantId
+  );
+
   const handleAddFavoriteRestaurants = () => {
-    addFavoriteRestaurants(userId, restaurantId).then((favoriteRestaurants) => {
-      if (favoriteRestaurants) {
-        dispatch(addToFavorites(favoriteRestaurants));
-      }
-    });
+    if (!isRestaurantAdded) {
+      addFavoriteRestaurants(userId, restaurantId).then(
+        (favoriteRestaurants) => {
+          if (favoriteRestaurants) {
+            dispatch(addToFavorites(favoriteRestaurants));
+          }
+        }
+      );
+    }
   };
   return (
     <button
       onClick={handleAddFavoriteRestaurants}
       className="bg-white p-3 rounded-full"
     >
-      <FaRegHeart size={20} />
+      {isRestaurantAdded ? (
+        <GoHeartFill size={20} color="red" />
+      ) : (
+        <FaRegHeart size={20} />
+      )}
     </button>
   );
 };
