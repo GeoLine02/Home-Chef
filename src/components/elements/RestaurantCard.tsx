003cb2bee3e197ff5497d0a6ca1cab63035be2c0
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-18 11:27:56 -0700

    change: id to restaurantID

--- src/components/elements/RestaurantCard.tsx
+++ src/components/elements/RestaurantCard.tsx
@@ -3,14 +3,14 @@ import RestaurantPhoto from "../../assets/restaurantPhoto.png";
 import routes from "../../constants/routes";
 import { RestaurantResultType } from "../../types/restaurant";
 
-const RestaurantCard = ({ id, name }: RestaurantResultType) => {
+const RestaurantCard = ({ name, restaurantID }: RestaurantResultType) => {
   const navigate = useNavigate();
   return (
     <div>
       <img
         className="cursor-pointer w-full"
         onClick={() => {
-          navigate(`${routes.home}${id}`);
+          navigate(`${routes.home}${restaurantID}`);
         }}
         src={RestaurantPhoto}
         alt="restaourantName"
