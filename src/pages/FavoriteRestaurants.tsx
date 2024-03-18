b5cba2c2f9084dd463b407d271892a72ebbb4f45
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-18 11:30:19 -0700

    add: data fethcing if favorites array is empty

--- src/pages/FavoriteRestaurants.tsx
+++ src/pages/FavoriteRestaurants.tsx
@@ -1,35 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/state/rootReducers";
-import { saveFavoriteRestaurantsData } from "../store/actions/actionCreator";
-import { useEffect } from "react";
-import { getFavoriteRestaurants } from "../api/index";
 import RestaurantCard from "../components/elements/RestaurantCard";
 import { IFavoriteRestaurants } from "../types/restaurant";
 import RemoveFromFavorites from "../components/elements/buttons/RemoveFromFavorites";
+import { useEffect } from "react";
+import { getFavoriteRestaurants } from "../api";
+import { saveFavoriteRestaurantsData } from "../store/actions/actionCreator";
 
 const FavoriteRestaurants = () => {
   const dispatch = useDispatch();
-  const userState = useSelector((state: RootState) => state.auth.userByID);
-  const userId = userState?.id;
-  const favoriteRestaurantsState = useSelector(
+  const favoriteRestaurants = useSelector(
     (state: RootState) => state.restaurants?.favoriteRestaurants
   );
+  const userState = useSelector((state: RootState) => state.auth.userByID);
+  const userId = userState?.id;
   useEffect(() => {
-    if (userId) {
-      getFavoriteRestaurants(userId).then((favoriteRestaurantsList) => {
-        dispatch(saveFavoriteRestaurantsData(favoriteRestaurantsList));
+    if (!favoriteRestaurants?.length && userId) {
+      getFavoriteRestaurants(userId).then((favoriteRestaurants) => {
+        dispatch(saveFavoriteRestaurantsData(favoriteRestaurants));
       });
     }
-  }, [dispatch, userId]);
+  }, [dispatch, userId, favoriteRestaurants?.length]);
 
   return (
     <div>
       <div>
         <h1 className="text-3xl font-medium">Favorite Restaurants</h1>
         <div className="flex gap-6 flex-wrap my-6">
-          {favoriteRestaurantsState?.map((restaurant: IFavoriteRestaurants) => (
+          {favoriteRestaurants?.map((restaurant: IFavoriteRestaurants) => (
             <div key={restaurant.id} className="relative">
               <RestaurantCard
+                restaurantID={restaurant.restaurantID}
                 id={restaurant.id}
                 name={restaurant.name}
                 address={restaurant.address}
