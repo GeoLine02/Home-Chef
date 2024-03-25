88b0718855cfe457e0897e89a72c980fe6cb6e8d
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-25 03:39:51 -0700

    fix: losing data when refreshing checkout page

--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -1,11 +1,42 @@
+import { useEffect } from "react";
 import CheckoutDeliveryTermsBox from "../components/blocks/CheckoutDeliveryTermsBox/CheckoutDeliveryTermsBox";
 import CheckoutOrderListBox from "../components/blocks/CheckoutOrderListBox/CheckoutOrderListBox";
 import CheckoutPaymentBox from "../components/blocks/CheckoutPaymentBox/CheckoutPaymentBox";
 import CheckoutPricesBox from "../components/blocks/CheckoutPricesBox/CheckoutPricesBox";
 import CheckoutHead from "../components/elements/CheckoutHead";
 import ContinueCheckoutBtn from "../components/elements/ContinueCheckoutBtn";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/state/rootReducers";
+import { http } from "../helpers/http";
+import { saveRestaurantByIdData } from "../store/actions/actionCreator";
 
 const CheckOut = () => {
+  const dispatch = useDispatch();
+  const restaurantByID = useSelector(
+    (state: RootState) => state.restaurants?.restaurantById
+  );
+  const cart = useSelector((state: RootState) => state.cart?.cart);
+  const restaurantID = cart[0]?.product?.restaurantID;
+
+  useEffect(() => {
+    const fetchRestaurantById = () => {
+      const apiCallOptions = {
+        headers: {
+          "content-type": "application/json",
+        },
+        method: "GET",
+      };
+      if (!restaurantByID?.length && restaurantID) {
+        http(`/restaurant/${restaurantID}`, apiCallOptions)
+          .then((restaurantByIDJson) => restaurantByIDJson.json())
+          .then((restaurantByID) => {
+            dispatch(saveRestaurantByIdData(restaurantByID));
+          });
+      }
+    };
+    fetchRestaurantById();
+  }, [restaurantByID.length, restaurantID, dispatch]);
+
   return (
     <main className="p-3 lg:py-6 bg-[#EEEEEE]">
       <div className="flex flex-col lg:flex-row justify-center items-start gap-8 w-full max-w-screen-2xl mx-auto">
