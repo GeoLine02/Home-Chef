5d4e30c476d53427cb0bab2aa6b10f4195205952
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-03-25 02:29:37 -0700

    add: total item cost calculator fucntion

--- src/components/elements/ContinueCheckoutBtn.tsx
+++ src/components/elements/ContinueCheckoutBtn.tsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import { text } from "../../helpers/functions";
+import { calculateItemTotalCost } from "../../helpers/totalCartCost";
+import { RootState } from "../../store/state/rootReducers";
 
 const ContinueCheckoutBtn = () => {
+  const cartState = useSelector((state: RootState) => state.cart.cart);
   return (
     <button className="px-6 h-12 my-4 cursor-pointer bg-orange-400 border border-transparent rounded-full w-full font-medium text-base">
-      {text("CHECKOUT_BTN")} 147$
+      {text("CHECKOUT_BTN")} {calculateItemTotalCost(cartState)}
     </button>
   );
 };
