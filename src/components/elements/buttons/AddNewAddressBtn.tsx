d73c7f6dcc3a0cfdac07d51b9c9dbad272a7c161
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-04-04 10:37:25 -0700

    add: toggleNewAddressModal action

--- src/components/elements/buttons/AddNewAddressBtn.tsx
+++ src/components/elements/buttons/AddNewAddressBtn.tsx
@@ -1,6 +1,15 @@
+import { useDispatch } from "react-redux";
+import { toggleAddNewAddressModalAction } from "../../../store/actions/actionCreator";
+
 const AddNewAddressBtn = () => {
+  const dispatch = useDispatch();
+
   return (
-    <button>
+    <button
+      onClick={() => {
+        dispatch(toggleAddNewAddressModalAction());
+      }}
+    >
       <u className="opacity-70">add new address</u>
     </button>
   );
