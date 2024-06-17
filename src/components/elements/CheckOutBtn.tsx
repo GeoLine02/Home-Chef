d2e070f0c39f5b27d638f2abb65a96c6a1beb407
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-06-17 17:03:08 +0400

    hotfix: cost calculation and restaurant card styling

--- src/components/elements/CheckOutBtn.tsx
+++ src/components/elements/CheckOutBtn.tsx
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/state/rootReducers";
 import { toggleAuthModal, toggleCart } from "../../store/actions/actionCreator";
 import { calculateItemTotalCost } from "../../helpers/totalCartCost";
-
+import { useEffect, useState } from "react";
 const CheckOutBtn = () => {
+  const [itemTotalCost, setItemTotalCost] = useState(0);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userVKinfo = useSelector(
@@ -15,27 +16,26 @@ const CheckOutBtn = () => {
     (state: RootState) => state.cart.isCartOpen
   );
   const cartState = useSelector((state: RootState) => state.cart.cart);
-
   const handleCheckOut = () => {
     if (toggleCartModal) {
       dispatch(toggleCart());
     }
-
     if (userVKinfo) {
       navigate(routes.checkOut);
     } else {
       dispatch(toggleAuthModal());
     }
   };
-
+  useEffect(() => {
+    calculateItemTotalCost(cartState).then((res) => setItemTotalCost(res));
+  });
   return (
     <button
       onClick={handleCheckOut}
       className="bg-orange-500 rounded-full w-full py-2 mt-6"
     >
-      Check out {calculateItemTotalCost(cartState)}
+      Check out {itemTotalCost || 0}
     </button>
   );
 };
-
 export default CheckOutBtn;
--- src/helpers/totalCartCost.ts
+++ src/helpers/totalCartCost.ts
@@ -1,7 +1,29 @@
 import { ProductQuantity } from "../types";
-
+import { getTotalAmount } from "../api/index";
 export const calculateItemTotalCost = (cartState: ProductQuantity[]) => {
-  return cartState
-    .map((item) => item.product.productPrice * item.quantity)
-    .reduce((current: number, accumulator: number) => current + accumulator, 0);
+  console.log("cartState", cartState);
+  const orderCalculateData = {
+    pickupLocation: {
+      latitude: 55.744089,
+      longitude: 37.546276,
+    },
+    deliveryLocation: {
+      latitude: 55.740674,
+      longitude: 37.625019,
+    },
+    orderKG: 25,
+    orderProducts: [
+      {
+        id: 1,
+        quantity: 2,
+      },
+      {
+        id: 4,
+        quantity: 1,
+      },
+    ],
+  };
+  return getTotalAmount(orderCalculateData)
+    .then((res) => res.json())
+    .then((res) => res);
 };
