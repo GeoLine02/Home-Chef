f80b4073431e5506e496945929017a37ece1e2f0
Marko <baghashvili.marko@gmail.com>
2024-06-20 15:11:41 +0400

    remove: persistor

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import App from "./App.tsx";
 import "./index.css";
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import rootReducers from "./store/state/rootReducers";
 import { Provider } from "react-redux";
 import "./i18n.ts";
-import { PersistGate } from "redux-persist/integration/react";
-import { composeWithDevTools } from "@redux-devtools/extension";
 
-const persistConfig = {
-  key: "route",
-  storage,
-  blacklist: ["auth", "sideBar", "search"],
-};
-
-// eslint-disable-next-line
-const persistedReducer = persistReducer(persistConfig, rootReducers as any);
-
-// Add your middleware here
-
-const store = createStore(
-  persistedReducer,
-  composeWithDevTools(applyMiddleware())
-);
-
-const persistor = persistStore(store);
+const store = createStore(rootReducers);
 
 store.subscribe(() => {
   console.log("current state", store.getState());
@@ -34,8 +14,6 @@ store.subscribe(() => {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>
 );
