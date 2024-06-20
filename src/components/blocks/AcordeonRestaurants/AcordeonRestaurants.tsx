3e249f5276569218db6b9f193d05f3e0eb400c94
Marko <baghashvili.marko@gmail.com>
2024-06-20 15:02:40 +0400

    build accordeon

--- src/components/blocks/AcordeonRestaurants/AcordeonRestaurants.tsx
+++ src/components/blocks/AcordeonRestaurants/AcordeonRestaurants.tsx
@@ -15,8 +15,10 @@ import "swiper/css";
 import { text } from "../../../helpers/functions";
 import { ToastContainer, toast } from "react-toastify";
 import i18n from "../../../i18n";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropup } from "react-icons/io";
 
-const FilteredRestaurantCategoryList = () => {
+const AcordeonRestaurants = () => {
   const dispatch = useDispatch();
   const restaurantCategories = useSelector(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,6 +27,9 @@ const FilteredRestaurantCategoryList = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [controlledSwiper, setControlledSwiper] = useState<any>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const [isAcordeonOpen, setIsAcordeonOpen] = useState(true);
+  const [activeId, setActiveId] = useState<number | null>(null);
 
   const showToastError = () => {
     toast.error("Restaurant categories fetching error");
@@ -65,52 +70,91 @@ const FilteredRestaurantCategoryList = () => {
   const handleClickPrev = () => controlledSwiper.slidePrev();
   const handleClickNext = () => controlledSwiper.slideNext();
 
+  const handleAcordeonTuggle = () => {
+    setIsAcordeonOpen((isAcordeonOpen) => !isAcordeonOpen);
+  };
+
   const numberOfSlides = newRestaurantCategories?.length;
   const slidesPerView = 12;
   const lastSlideIndex = numberOfSlides - slidesPerView;
 
   return (
-    <div>
-      <h1>{text("FILTERED_RSTRNT_CTG_LST_HEADING")}</h1>
-      <Swiper
+    <div
+      className="mt-4 flex flex-col"
+      style={{ height: "calc(100vh - 13.75rem)" }}
+    >
+      <div
+        onClick={handleAcordeonTuggle}
+        className="mb-4 flex gap-2 items-center w-100 cursor-pointer"
+      >
+        {isAcordeonOpen ? (
+          <>
+            <h1>{text("FILTERED_RSTRNT_CTG_LST_HEADING")}</h1>
+            <span>
+              <IoMdArrowDropdown className="size-5" />
+            </span>
+          </>
+        ) : (
+          <>
+            <h1>{text("FILTERED_RSTRNT_CTG_LST_HEADING")}</h1>
+            <span>
+              <IoMdArrowDropup className="size-5" />
+            </span>
+          </>
+        )}
+      </div>
+      {/* <Swiper
         slidesPerView={slidesPerView}
         modules={[Controller]}
         controller={{ control: controlledSwiper }}
         onSwiper={setControlledSwiper}
-        className=" gap-3 whitespace-nowrap overflow-x-auto lg:w-full lg:mx-auto lg:overflow-x-hidden md:min-w-[768px]"
+        className="flex gap-3 whitespace-nowrap overflow-x-auto lg:w-full lg:mx-auto lg:overflow-x-hidden md:min-w-[768px]"
+      > */}
+      {/* <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        closeOnClick
+        draggable
+        theme="light"
+      /> */}
+      <div
+        className={`flex-grow overflow-y-auto transition-all duration-300 ease-in-out pt-2 ${
+          isAcordeonOpen
+            ? "max-h-full"
+            : "max-h-0 opacity-0 pointer-events-none"
+        }`}
       >
-        <ToastContainer
-          position="top-left"
-          autoClose={3000}
-          closeOnClick
-          draggable
-          theme="light"
-        />
         {newRestaurantCategories?.map((food: any, index: number) => {
           return (
             <SwiperSlide
               key={index}
-              className="flex flex-col justify-center items-center cursor-pointer"
+              className={`flex justify-start h-max gap-8 items-center cursor-pointer ${
+                activeId === food.id
+                  ? "border-r-4 border-r-gray-600 bg-gray-100"
+                  : "" // Apply red border if this item is active
+              }`}
+              onClick={() => {
+                setActiveId(food.id); // Set the active ID
+                dispatch(selectCategoryID(food.id));
+              }}
             >
               <img
                 className="min-w-16 min-h-16"
                 src={food.icon}
                 alt={food.typeName}
-                onClick={() => dispatch(selectCategoryID(food.id))}
               />
               <span>{food.typeName}</span>
             </SwiperSlide>
           );
         })}
-        {activeIndex !== lastSlideIndex && (
-          <ScrollBtn role="right" onClick={handleClickNext} />
-        )}
-        {activeIndex !== 0 && (
-          <ScrollBtn role="left" onClick={handleClickPrev} />
-        )}
-      </Swiper>
+      </div>
+      {/* {activeIndex !== lastSlideIndex && (
+        <ScrollBtn role="right" onClick={handleClickNext} />
+      )}
+      {activeIndex !== 0 && <ScrollBtn role="left" onClick={handleClickPrev} />} */}
+      {/* </Swiper> */}
     </div>
   );
 };
 
-export default FilteredRestaurantCategoryList;
+export default AcordeonRestaurants;
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,3 +16,53 @@ export default {
   },
   plugins: [],
 };
+
+
+/* 
+::-webkit-scrollbar {
+  width: 1.6rem;
+}
+
+::-webkit-scrollbar-corner,
+::-webkit-scrollbar-track {
+  background: var(--color-neutral-700);
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--color-neutral-500);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: var(--color-neutral-400);
+}
+
+::-webkit-scrollbar-thumb:active {
+  background: var(--color-neutral-300);
+}
+
+
+
+::-webkit-scrollbar-button {
+  display: block;
+  background-color: var(--color-neutral-700);
+  background-repeat: no-repeat;
+  background-size: 50%;
+  background-position: center;
+}
+
+::-webkit-scrollbar-button:vertical:end:increment {
+  background-image: url('https://upload.wikimedia.org/wikipedia/commons/e/ee/Chevron-down.svg'); 
+}
+
+::-webkit-scrollbar-button:vertical:end:decrement {
+  display: none; 
+}
+
+ ::-webkit-scrollbar-button:vertical:start:increment {
+  display: none; 
+}
+
+::-webkit-scrollbar-button:vertical:start:decrement {
+  background-image: url('https://upload.wikimedia.org/wikipedia/commons/7/7e/Chevron-up.svg');
+}
+ */
